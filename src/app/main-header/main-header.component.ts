846a3ef841c7887a0ec94a51f6071616915e2dee
cleon1055 <cleon1055@gmail.com>
2020-04-29 15:57:14 -0700

    Added Code

---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+
+
+  firstName: string;
+  lastName: string;
+  constructor() {
+    this.firstName = 'Cesar';
+    this.lastName = 'Leon';
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  showGreeting() {
+    const row2 = document.getElementById('r1c2');
+    row2.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+  }
+
+  resetGreeting() {
+    const row2 = document.getElementById('r1c2');
+    row2.innerHTML = 'r1c2';
+
+  }
+
+}
